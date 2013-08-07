19b620c0a51f1ecbd061b492d5b5fe5ab659cc5f
rankun203 <rankun203@gmail.com>
2013-08-07 20:03:52 +0800

    增加进度记录，实现断点续下功能。

--- YoudaoOffline/src/com/mindfine/youdaooffline/OfflineActor.java
+++ YoudaoOffline/src/com/mindfine/youdaooffline/OfflineActor.java
@@ -1,7 +1,9 @@
 package com.mindfine.youdaooffline;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,35 +16,15 @@ import com.mindfine.youdaodict.pronouncer.YoudaoPronouncer;
 
 
 public class OfflineActor {
+	public static boolean debug = true;
 	public static String baseDir = System.getProperty("user.dir");
 			
 	public static void main(String[] args) throws Exception {
 		
 		OfflineActor oa = new OfflineActor();
-		String [] words = oa.parseWords("words.txt");
+		oa.parseAndSaveWords("words.txt");
 		
-		FileWriter fw = new FileWriter(baseDir + "collinsWords.txt", true);
-		PrintWriter pw = new PrintWriter(fw);
-		
-		oa.saveWordsAndAudio(pw, words);
-		pw.close();
 	}
-	/**
-	 * 下载词典信息和音频信息
-	 * @param pw 输出词典信息的位置
-	 * @param words 当前操作的单词
-	 */
-	public void saveWordsAndAudio(PrintWriter pw, String[] words) {
-		for(int i = 0; i < words.length; i++) {
-			String word = words[i];
-			YoudaoCollinsFetcher ycf = new YoudaoCollinsFetcher();
-			ycf.setStyleType(Fetcher.StyleType.plain);
-			String exp = ycf.jsoupFetcher(word);
-			printToFile(word, exp, pw);
-			saveAudio(word);
-		}
-	}
-	
 	/**
 	 * @param word
 	 */
@@ -57,13 +39,14 @@ public class OfflineActor {
 	 * 即：```分隔不同的单词，``分隔单词内的词汇和解释
 	 * @param exp 要打印的单词及释义
 	 * @param word 当前打印的单词
+	 * @param pw 往哪儿打印？
 	 */
 	public void printToFile(String word, String exp, PrintWriter pw) {
 		File collinsWords = new File(baseDir + "collinsWords.txt");
 		try {
 			if(!collinsWords.exists()) {
 					if(!collinsWords.createNewFile()) {
-						System.err.println("输出数据的文件(user.dir.collinsWords.txt)不存在，且无法创建！");
+						System.err.println("输出数据的文件(user.dir/collinsWords.txt)不存在，且无法创建！");
 					}
 			}
 			pw.print(word + "``");
@@ -74,13 +57,11 @@ public class OfflineActor {
 		}
 	}
 	/**
-	 * 警告！该方法有副作用，会将单词文件写入一个临时文件中<br>
-	 * 该临时文件位于当前工作路径中，名称为：words-tmp.txt
 	 * @param filePath 要解析哪个文件的单词？
 	 * @return 单词们和他们是否已经获取释义完成的标记
 	 * @throws Exception 
 	 */
-	public String[] parseWords(String filePath) throws Exception{
+	public String[] parseAndSaveWords(String filePath) throws Exception{
 		File wf = new File(filePath);
 		String [] wds = null;
 		if(wf.exists()) {
@@ -90,6 +71,7 @@ public class OfflineActor {
 			while((ch = fis.read()) != -1) {
 				sb.append((char)ch);
 			}
+			fis.close();
 			String tmpString = new String(sb);
 			String words = new String(tmpString.getBytes("ISO8859-1"), "utf-8");
 			wds = words.split("[^a-zA-z]");
@@ -100,19 +82,60 @@ public class OfflineActor {
 				noDupli.add(wds[i]);
 			}
 			
-			PrintWriter pw = new PrintWriter(new FileWriter(System.getProperty("user.dir") + "/words-tmp.txt"));
-			String tpr = null;
-			Iterator<String> noDupIterator = noDupli.iterator();
-			while(noDupIterator.hasNext()) {
-				tpr = noDupIterator.next();
-				if(!tpr.equals("")) {
-					pw.println(tpr);
+			
+			//读取已经完成的单词
+			File finishedFile = new File(System.getProperty("user.dir") + "/words-finished.txt");
+			String finishedWords = null;
+			if(finishedFile.exists()) {
+				BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/words-finished.txt"));
+				StringBuilder finishedSB = new StringBuilder();
+				String bufLine = null;
+				while((bufLine = br.readLine()) != null) {
+					finishedSB.append(bufLine);
 				}
+				finishedWords = new String(finishedSB);
+				br.close();
 			}
 			
-			pw.close();
+			//已完成单词文件，以追加方式打开
+			PrintWriter finishedLogPw = new PrintWriter(new FileWriter(System.getProperty("user.dir") + "/words-finished.txt", true));
+			String tprWord = null;
+			Iterator<String> noDupIterator = noDupli.iterator();
+
+			//存储单词释义的文件
+			FileWriter fw = new FileWriter(baseDir + "collinsWords.txt", true);
+			PrintWriter expOutPw = new PrintWriter(fw);
+			
+			while(noDupIterator.hasNext()) {
+				tprWord = noDupIterator.next();
+if(debug == true) System.out.print("当前正在下载:" + tprWord + "...");
+				if(finishedWords != null && !finishedWords.equals("") && finishedWords.contains(tprWord)) {
+if(debug == true) System.out.println("已经存在");
+					continue;
+				}
+				//先获取释义及存储释义音频
+				//然后存储进度
+				if(!tprWord.equals("")) {
+					YoudaoCollinsFetcher ycf = new YoudaoCollinsFetcher();
+					ycf.setStyleType(Fetcher.StyleType.plain);
+					String exp = ycf.jsoupFetcher(tprWord);
+					if(null == exp || exp.equals("")) {
+						continue;
+					}
+					printToFile(tprWord, exp, expOutPw);
+					saveAudio(tprWord);
+
+					finishedLogPw.println(tprWord);
+				}
+				expOutPw.flush();
+				finishedLogPw.flush();
+if(debug == true) System.out.println("done");
+			}
+			
+			expOutPw.close();
+			finishedLogPw.close();
 		} else {
-			System.out.println("File not exists.");
+			System.out.println("File " + filePath + " not exists.");
 		}
 		return wds;
 	}
