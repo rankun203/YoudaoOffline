38a61c27bc2bfd2f3248dd0f4b142765e6ebfb14
rankun203 <rankun203@gmail.com>
2013-08-11 22:51:14 +0800

    修改DicReader类，测试按行读取和按字节读取的性能

--- YoudaoOffline/src/com/mindfine/youdaooffline/DicReader.java
+++ YoudaoOffline/src/com/mindfine/youdaooffline/DicReader.java
@@ -1,15 +1,23 @@
 package com.mindfine.youdaooffline;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class DicReader {
 	public static void main(String[] args) throws Exception {
-		int start = 286161;
-		int length = 22;
-		getLinesFrom(start, length);
+		int start = 286113;
+		int length = 6;
+//		getLinesFrom(start, length);
+		getExplainFrom(14700135, 335);
 	}
 
+	/**
+	 * 按行读取解释
+	 * @param start 读取起始行数
+	 * @param length 读取行数
+	 * @throws Exception 抛出Exception
+	 */
 	private static void getLinesFrom(int start, int length) throws Exception {
 		long startt = System.currentTimeMillis();
 		FileReader fr = new FileReader(System.getProperty("user.dir") + "/youdaocollins.exp");
@@ -29,6 +37,22 @@ public class DicReader {
 		
 		System.out.println(explain.toString());
 		System.out.println("expend mills = " + (endt - startt));
+		br.close();
+		fr.close();
+	}
+	
+	public static void getExplainFrom(long skip, int length) throws Exception{
+		long startt = System.currentTimeMillis();
+		FileReader fr = new FileReader(System.getProperty("user.dir") + "/youdaocollins.exp");
+		fr.skip(skip);
+		char[] temp = new char[length];
+		fr.read(temp, 0, length);
+
+		long endt = System.currentTimeMillis();
+		System.out.println(new String(temp));
+		System.out.println("expend mills = " + (endt - startt));
+		
+		fr.close();
 	}
 	
 }
